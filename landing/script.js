8bd31733f865953c455e0894c685d3874a9ff67b
arymus <arymusr@gmail.com>
2025-12-17 12:13:07 -0500

    Started working on projects section

--- landing/script.js
+++ landing/script.js
@@ -2,15 +2,72 @@ async function getRepos() {
 
     // Try to run the block of code below, catching and handling errors
     try {
-        const url = ""; // The GitHub API to pull repositories
+        const url = "https://api.github.com/users/arymus/repos"; // The GitHub API to pull repositories
         const res = await fetch(url); // Send a GET request to the URL (await pauses function until fetch completes)
-        if (res.status != 200) throw new Error("HTTP Error: " + res.statusText); // If the response is not 200 (OK), throw an error
+        if (res.status != 200) throw new Error("HTTP Error: " + res.status); // If the response is not 200 (OK), throw an error
     } catch (e) { alert(e); } // Alert any errors that are caught
 
     const data = await res.json(); // Convert the response body to a JavaScript object (await pauses function until JSON parsing completes)
     return data; // Return the data
 }
 
-async function displayRepositories() {
-    const repos = await getRepos(); // Call the getRepos function (await pauses function until we get the data)
+async function getLangs(repo) {
+    try {
+        const res = await fetch(repo.languages_url);
+        if (res.status != 200) throw new Error("HTTP Error: " + res.status);
+    } catch (e) { alert(e); }
+
+    const data = await res.json();
+    const languages = Object.keys(data); // Get the keys of the object returned from the API (the languages, the API has a language and the amoutn of lines associated with it)
+    return languages;
 }
+
+async function displayRepos() {
+    const repos = await getRepos(); // Call the function that gets the repositories from the GitHub API (the repos come as an array of objects)
+
+    for (let repo of repos) { // Iterate over each repository
+        const repoContainer = document.createElement("div");
+        repoContainer.classList.add("repo"); // Give the div a class of .repo
+
+        const repoContainerStyles = {
+            backgroundColor: "#FFFFFF",
+            margin: "20px",
+            padding: "10px",
+            border: "2px solid #000000",
+            borderRadius: "16px",
+
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }
+
+        Object.assign(repoContainer.style, repoContainerStyles);
+
+        const repoInfo = { // Create an object with information from the API
+            name: repo.name,
+            desc: repo.description,
+            url: repo.html_url,
+            lang: await getLangs(repo),
+        };
+
+        const nameText = document.createElement("a");
+        nameText.innerText = repoInfo.name;
+        nameText.href = repoInfo.url;
+
+        const descText = document.createElement("p");
+        descText.innerText = repoInfo.desc === null ? "[No description]" : repoInfo.desc; // If the repository has no description is null (has no desc), then put [No description]. If else, put the description
+
+        const langText = document.createElement("p");
+        langText.classList.add("langs"); // Add a class of .langs to the p element
+        for (i in lang) langText.innerText += lang[i] + ", ";  // Add the language to the element's innerText (plus a comma)
+
+        // Append all the newly made elements to the repo container and then append the repo container to the projects section
+        repoContainer.appendChild(nameText);
+        repoContainer.appendChild(descText);
+        repoContainer.appendChild(langText);
+        document.querySelector(".repositories").appendChild(repoContainer);
+    }
+}
+
+displayRepos();
