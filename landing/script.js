5a370f9ebee230014ec53b71a3d0de3cadbdaa7a
arymus <arymusr@gmail.com>
2025-12-17 16:55:04 -0500

    Fetched and displayed repositories

--- landing/script.js
+++ landing/script.js
@@ -1,34 +1,44 @@
 async function getRepos() {
-
-    // Try to run the block of code below, catching and handling errors
-    try {
+    try { // Try to run the block of code below, catching and handling errors
         const url = "https://api.github.com/users/arymus/repos"; // The GitHub API to pull repositories
         const res = await fetch(url); // Send a GET request to the URL (await pauses function until fetch completes)
-        if (res.status != 200) throw new Error("HTTP Error: " + res.status); // If the response is not 200 (OK), throw an error
+        if (res.status != 200) throw new Error(`HTTP Status ${res.status} ${res.statusText}`); // If the response is not 200 (OK), throw an error
+        const data = await res.json(); // Convert the response body to a JavaScript object (await pauses function until JSON parsing completes)
+        return data; // Return the data
     } catch (e) { alert(e); } // Alert any errors that are caught
-
-    const data = await res.json(); // Convert the response body to a JavaScript object (await pauses function until JSON parsing completes)
-    return data; // Return the data
+    
 }
 
 async function getLangs(repo) {
     try {
         const res = await fetch(repo.languages_url);
-        if (res.status != 200) throw new Error("HTTP Error: " + res.status);
+        if (res.status != 200) throw new Error(`HTTP Status ${res.status} ${res.statusText}`);
+        const data = await res.json();
+        const languages = Object.keys(data); // Get the keys of the object returned from the API (the languages, the API has a language and the amoutn of lines associated with it)
+        return languages;
     } catch (e) { alert(e); }
+}
 
-    const data = await res.json();
-    const languages = Object.keys(data); // Get the keys of the object returned from the API (the languages, the API has a language and the amoutn of lines associated with it)
-    return languages;
+function parseName(name) {
+  const words = name.split("-"); // Split the name string into words
+  
+  // Capitalize the first letter of each word and leave the rest of the word in lowercase
+  const parsedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+  
+  return parsedWords.join(" "); // Join the words back together with a space between them
 }
 
 async function displayRepos() {
     const repos = await getRepos(); // Call the function that gets the repositories from the GitHub API (the repos come as an array of objects)
-
+    
+    console.log(repos);
     for (let repo of repos) { // Iterate over each repository
         const repoContainer = document.createElement("div");
         repoContainer.classList.add("repo"); // Give the div a class of .repo
 
+        // Styles for the repository container
         const repoContainerStyles = {
             backgroundColor: "#FFFFFF",
             margin: "20px",
@@ -45,7 +55,7 @@ async function displayRepos() {
         Object.assign(repoContainer.style, repoContainerStyles);
 
         const repoInfo = { // Create an object with information from the API
-            name: repo.name,
+            name: parseName(repo.name),
             desc: repo.description,
             url: repo.html_url,
             lang: await getLangs(repo),
@@ -60,7 +70,7 @@ async function displayRepos() {
 
         const langText = document.createElement("p");
         langText.classList.add("langs"); // Add a class of .langs to the p element
-        for (i in lang) langText.innerText += lang[i] + ", ";  // Add the language to the element's innerText (plus a comma)
+        for (i in repoInfo.lang) langText.innerText += repoInfo.lang[i] + ", ";  // Add the language to the element's innerText (plus a comma)
 
         // Append all the newly made elements to the repo container and then append the repo container to the projects section
         repoContainer.appendChild(nameText);
